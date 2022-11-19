09d8c9d15c6ff4d920472879058ab17a1f8756d3
ARYAN SINGLA <72137517+aryansingla@users.noreply.github.com>
2022-11-19 09:29:33 +0530

    router done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import JobList from './components/jobList/JobList';
+import LandingPage from './components/landingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-     Hello
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<LandingPage />}/>
+        <Route exact path="jobs" element={<JobList />} />
+    </Routes>
+  </BrowserRouter>
+  </div>
   );
 }
 
