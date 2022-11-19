abf732f4c87a87e0cfbaee966d3bdc7168fd2081
simrandhankar <simran.dhankar@itmtb.com>
2022-11-19 14:41:36 +0530

    sim2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,27 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import BountyImg from "../assets/images/Bounty_logo.png";
 function Navbar() {
   return (
     <div className="flex justify-between h-14 pt-3 pl-2 pr-2 font-semibold text-white bg-[#AF69EE]">
-      <div className="">logo</div>
+      <div className="">
+        <Link>
+          <img
+            src={BountyImg}
+            alt="logo"
+            style={{ width: "25%", marginTop: "-16px" }}
+          />
+        </Link>
+      </div>
       <div className="mid">
         <ul className="flex">
           <Link>
-            <li className="px-10">Home</li>
+            <li className="px-10 hover:text-black">Home</li>
           </Link>
           <Link>
             {" "}
-            <li className="px-10">Employer</li>
+            <li className="px-10 hover:text-black">Employer</li>
           </Link>
           <Link>
             {" "}
-            <li className="px-10">Candidate</li>
+            <li className="px-10 hover:text-black">Candidate</li>
           </Link>
         </ul>
       </div>
       <div className="flex w-18 right">
         <Link>
-          <div className="pr-10 ">Login</div>
+          <div className="pr-10 hover:text-black">Login</div>
         </Link>
 
         <button className=" w-28 rounded-full mb-2 -mt-1 hover:bg-opacity-80 bg-[#EF5DA8]">
--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import GirlImg from "../../assets/images/GirlImg.png"
 const LandingPage = () => {
   return (
-    <div className='pt-4'>LandingPage</div>
+    <div className='flex pt-4'>
+      <div className=""><h1 className='text-5xl font-bold '>Find the bug and get your bounty</h1>
+<p className='w-40 '>Find You New Job Today! New Job Postings Everyday just for you, browse the job you want and apply wherever you want</p>
+<div className=''><h1>Recently occured bugs:</h1>
+<ul>
+  <li>Bug1</li>
+  <li>Bug2</li>
+  <li>Bug3</li>
+  </ul>
+  </div>
+</div>
+      <div className="">
+        <img src={GirlImg} alt="right-image" />
+      </div>
+    </div>
   )
 }
 
