860e7a23f714bfd2095f1c8d8c5a9f2cdac5fb35
simrandhankar <simran.dhankar@itmtb.com>
2022-11-19 14:47:41 +0530

    sim3

--- src/components/landingPage/LandingPage.jsx
+++ src/components/landingPage/LandingPage.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 import GirlImg from "../../assets/images/GirlImg.png"
 const LandingPage = () => {
   return (
-    <div className='flex pt-4'>
+    <div className='flex pt-4 '>
       <div className=""><h1 className='text-5xl font-bold '>Find the bug and get your bounty</h1>
-<p className='w-40 '>Find You New Job Today! New Job Postings Everyday just for you, browse the job you want and apply wherever you want</p>
+<p className='pt-8 w-80'>Find You New Job Today! New Job Postings Everyday just for you, browse the job you want and apply wherever you want</p>
 <div className=''><h1>Recently occured bugs:</h1>
 <ul>
   <li>Bug1</li>
