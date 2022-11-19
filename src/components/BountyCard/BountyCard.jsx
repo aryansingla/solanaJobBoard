0bedea9113fa58ae495147bfd7b1216b8036449a
ARYAN SINGLA <72137517+aryansingla@users.noreply.github.com>
2022-11-19 14:40:18 +0530

    changes

--- src/components/BountyCard/BountyCard.jsx
+++ src/components/BountyCard/BountyCard.jsx
@@ -3,9 +3,13 @@ import React from 'react'
 const BountyCard = ({name,img,heading,text}) => {
   return (
     <div className='bountyCard'>
+        <div className="details" style={{display:"flex" , justifyContent:"space-between"}}>
         <h5>{name}</h5>
+        <button style={{width:"80px",height:"30px",marginTop:"50px",backgroundColor:"#4128ad",borderStyle:"none",borderRadius:"8px",color:"white"}}>Get details</button>
+        </div>
         <div className="details" style={{display:"flex"}}>
         <img src={img} alt="" className='bountyLogo' style={{width:"120px"}}/>
+        <h4>{heading}</h4>
         <h4>{heading}</h4>
         </div> 
         <p>{text}</p>
--- src/data.js
+++ src/data.js
@@ -6,6 +6,7 @@ export const list = [
     img: amazon,
     heading: "Web3 React Components By Covalenet Bounty",
     text: "Components should be built as functional components supporting real-world use cases and incorporate one or more Covalent API endpoints. They should leverage popular UI design libraries such as antd and be cleanly written and adequately tested. They can take the form of tables, charts, cards, etc. Please see the existing web3 components and contribution guidelines as reference.",
+    language:"Javascript,React,MysQl"
   },
   {
     id: 2,
@@ -13,6 +14,7 @@ export const list = [
     img: amazon,
     heading: "Web3 React Components By Covalenet Bounty",
     text: "Components should be built as functional components supporting real-world use cases and incorporate one or more Covalent API endpoints. They should leverage popular UI design libraries such as antd and be cleanly written and adequately tested. They can take the form of tables, charts, cards, etc. Please see the existing web3 components and contribution guidelines as reference.",
+    language:"Javascript,React,MysQl",
   },
   {
     id: 3,
@@ -20,5 +22,6 @@ export const list = [
     img: amazon,
     heading: "Web3 React Components By Covalenet Bounty",
     text: "Components should be built as functional components supporting real-world use cases and incorporate one or more Covalent API endpoints. They should leverage popular UI design libraries such as antd and be cleanly written and adequately tested. They can take the form of tables, charts, cards, etc. Please see the existing web3 components and contribution guidelines as reference.",
+    language:"Javascript,React,MysQl",
   },
 ];
